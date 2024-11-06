708d5324f7af371dee93dec2d8c7dc97bbb7c045
Fabio Leonardo <leonardofafj@gmail.com>
2024-11-06 12:10:26 -0300

    Update no header component

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -3,16 +3,31 @@ import styles from "./header.module.css";
 import slack from "../../public/images/header/slack.webp"
 import profilepic from "../../public/images/header/perfil.webp"
 import notif from "../../public/images/header/notificacao.webp"
+import filter from "../../public/images/header/filter.svg"
+import magglass from "../../public/images/header/search.webp"
 const Header = () =>{
     return <div className={ styles.container }>
         <header>
                 <h1 className={styles.title}>Liferay User Group</h1>
 
                 <div className={styles["search-bar"]}>		
-                    <div className={styles["search-bar-icon"]}>
-                        {/* adicionar botao de filtragem */}
-                    </div>
+                    <button className={styles["search-bar-icon"]}>
+                        <Image 
+                            src={filter}
+                            alt="Filtros"
+                            width={20}
+                            height={20}
+                        />
+                    </button>
                     <input id="search" type="text" className={styles["search-element"]} placeholder="Pesquisar"/>
+                    <button className={styles["search-button"]}>
+                        <Image 
+                            src={magglass}
+                            alt="Pesquisar"
+                            width={20}
+                            height={20}
+                        />
+                    </button>
                 </div>
 
                 <div className={styles["header-elements"]}>
