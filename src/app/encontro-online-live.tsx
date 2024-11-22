bb667b0fe5a04b2bae50cbc07b198c2e1f16c54e
CaioP <caiopedro28@gmai.com>
2024-11-22 10:12:35 -0300

    ajeitando o import

--- src/app/encontro-online-live.tsx
+++ src/app/encontro-online-live.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../components/header/page';
 import Content from '../../components/EncontroOnline/content/content';
 import Chat from '../../components/EncontroOnline/chat/chat';
-import '../';
 
 const App: React.FC = () => {
   return (
