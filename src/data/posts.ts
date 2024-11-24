97dc6aa66c44d76a4a7078c08e4cfd37df93bf6a
Fabio Leonardo <leonardofafj@gmail.com>
2024-11-24 00:20:04 -0300

    change Post Content interface

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,72 +1,14 @@
-import { profile } from "console";
-
-interface User {
+export interface UserData {
   id: number;
   username: string;
   profilePic: string;
 }
 
-interface Post {
+export interface PostContent {
   id: number;
-  author: User;
+  userId: number;
   // date: Date;
   title: string;
   content: string;
   comments: number;
 }
-
-export const users = [
-  {
-    id: 1,
-    username: "Fábio",
-    profilePic: "",
-  },
-  {
-    id: 2,
-    username: "Leonardo",
-    profilePic: "",
-  },
-];
-
-export const posts = [
-  {
-    id: 1,
-    author: users.at(0),
-    // date: Date.parse("12/02/2002"),
-    title: "Primeiro post aeeee",
-    content:
-      "Primeiro post de teste para conferir se deu certo a implementação de posts baseado no array em data/posts.ts ,lorem ipsum dolor sit amet aasddsadsadsadsasad ",
-    coments: 300,
-    likes: 10992,
-  },
-  {
-    id: 2,
-    author: users.at(0),
-    // date: Date.parse("12/02/2002"),
-    title: "Segundo post",
-    content:
-      "Segundo post para testar se a funcionalidade de colocar mais posts baseado no array de posts está funcionando",
-    coments: 4,
-    likes: 18000,
-  },
-  {
-    id: 3,
-    author: users.at(1),
-    // date: Date.parse("12/02/2002"),
-    title: "Terceiro post",
-    content:
-      "Terceiro post para testar se a funcionalidade de colocar mais posts baseado no array de posts está funcionando",
-    coments: 3,
-    likes: 1800,
-  },
-  {
-    id: 4,
-    author: users.at(1),
-    // date: Date.parse("12/02/2002"),
-    title: "Quarto post",
-    content:
-      "Quarto post para testar se a funcionalidade de colocar mais posts baseado no array de posts está funcionando",
-    coments: 113,
-    likes: 19000,
-  },
-];
