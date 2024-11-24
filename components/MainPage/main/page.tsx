8f187405ce8fdc408fa2c6bbd287e18b5bc7ecd3
Fabio Leonardo <leonardofafj@gmail.com>
2024-11-24 00:21:54 -0300

    add post components

--- components/MainPage/main/page.tsx
+++ components/MainPage/main/page.tsx
@@ -1,153 +1,43 @@
+import useFetch from "@/useFetch";
+import Post from "../post/page";
 import styles from "./main.module.css";
+import { PostContent } from "@/data/posts";
+import Modal from "../modal/page";
+import { useState } from "react";
 const Main = () => {
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useFetch<PostContent[]>("http://localhost:8000/posts");
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleToggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <main className={styles.container}>
       <div className={styles["post-container"]}>
         <h2 className={styles.title}>Postagens</h2>
-        <ul className={`${styles["post-list"]} hide-scroll`}>
-          <li className={styles.post}>
-            <div className={styles["post-wrapper"]}>
-              <header className={styles["post-header"]}>
-                <div className={styles["post-author"]}>
-                  <img src="/" alt="" className={styles["profile-pic"]} />
-                  <span className={styles.username}>Nome do autor</span>
-                  {/* <span>Hora</span> */}
-                </div>
-                {/* <div>
-                  Seguir e mais
-                </div> */}
-              </header>
-              <div className={styles["post-content-container"]}>
-                <h3 className={styles["post-title"]}>Título do post</h3>
-                <p className={styles["post-content"]}>
-                  Conteudo do post: Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Voluptatum sunt quos illo, sed sit
-                  aspernatur ullam perferendis quidem excepturi delectus iusto
-                  architecto suscipit tempora porro dignissimos atque hic
-                  voluptates fugiat. Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Error, quod! Similique, doloribus veritatis
-                  laudantium eaque repellat saepe perspiciatis numquam veniam
-                  nulla rem. Odio exercitationem voluptatibus minus earum ex
-                  delectus blanditiis.lorem Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Harum amet ullam rem fuga porro
-                  ducimus. Quas recusandae nesciunt, eveniet alias odio at quod
-                  ea obcaecati voluptate possimus quo consectetur aspernatur!
-                </p>
-              </div>
-              <div className={styles["post-stats"]}>
-                <span>like</span>
-                <span>comment</span>
-              </div>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <div className={styles["post-wrapper"]}>
-              <header className={styles["post-header"]}>
-                <div className={styles["post-author"]}>
-                  <img src="/" alt="" className={styles["profile-pic"]} />
-                  <span className={styles.username}>Nome do autor</span>
-                  {/* <span>Hora</span> */}
-                </div>
-                {/* <div>
-                  Seguir e mais
-                </div> */}
-              </header>
-              <div className={styles["post-content-container"]}>
-                <h3 className={styles["post-title"]}>Título do post</h3>
-                <p className={styles["post-content"]}>
-                  Conteudo do post: Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Voluptatum sunt quos illo, sed sit
-                  aspernatur ullam perferendis quidem excepturi delectus iusto
-                  architecto suscipit tempora porro dignissimos atque hic
-                  voluptates fugiat. Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Error, quod! Similique, doloribus veritatis
-                  laudantium eaque repellat saepe perspiciatis numquam veniam
-                  nulla rem. Odio exercitationem voluptatibus minus earum ex
-                  delectus blanditiis.lorem Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Harum amet ullam rem fuga porro
-                  ducimus. Quas recusandae nesciunt, eveniet alias odio at quod
-                  ea obcaecati voluptate possimus quo consectetur aspernatur!
-                </p>
-              </div>
-              <div className={styles["post-stats"]}>
-                <span>like</span>
-                <span>comment</span>
-              </div>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <div className={styles["post-wrapper"]}>
-              <header className={styles["post-header"]}>
-                <div className={styles["post-author"]}>
-                  <img src="/" alt="" className={styles["profile-pic"]} />
-                  <span className={styles.username}>Nome do autor</span>
-                  {/* <span>Hora</span> */}
-                </div>
-                {/* <div>
-                  Seguir e mais
-                </div> */}
-              </header>
-              <div className={styles["post-content-container"]}>
-                <h3 className={styles["post-title"]}>Título do post</h3>
-                <p className={styles["post-content"]}>
-                  Conteudo do post: Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Voluptatum sunt quos illo, sed sit
-                  aspernatur ullam perferendis quidem excepturi delectus iusto
-                  architecto suscipit tempora porro dignissimos atque hic
-                  voluptates fugiat. Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Error, quod! Similique, doloribus veritatis
-                  laudantium eaque repellat saepe perspiciatis numquam veniam
-                  nulla rem. Odio exercitationem voluptatibus minus earum ex
-                  delectus blanditiis.lorem Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Harum amet ullam rem fuga porro
-                  ducimus. Quas recusandae nesciunt, eveniet alias odio at quod
-                  ea obcaecati voluptate possimus quo consectetur aspernatur!
-                </p>
-              </div>
-              <div className={styles["post-stats"]}>
-                <span>like</span>
-                <span>comment</span>
-              </div>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <div className={styles["post-wrapper"]}>
-              <header className={styles["post-header"]}>
-                <div className={styles["post-author"]}>
-                  <img src="/" alt="" className={styles["profile-pic"]} />
-                  <span className={styles.username}>Nome do autor</span>
-                  {/* <span>Hora</span> */}
-                </div>
-                {/* <div>
-                  Seguir e mais
-                </div> */}
-              </header>
-              <div className={styles["post-content-container"]}>
-                <h3 className={styles["post-title"]}>Título do post</h3>
-                <p className={styles["post-content"]}>
-                  Conteudo do post: Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Voluptatum sunt quos illo, sed sit
-                  aspernatur ullam perferendis quidem excepturi delectus iusto
-                  architecto suscipit tempora porro dignissimos atque hic
-                  voluptates fugiat. Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Error, quod! Similique, doloribus veritatis
-                  laudantium eaque repellat saepe perspiciatis numquam veniam
-                  nulla rem. Odio exercitationem voluptatibus minus earum ex
-                  delectus blanditiis.lorem Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Harum amet ullam rem fuga porro
-                  ducimus. Quas recusandae nesciunt, eveniet alias odio at quod
-                  ea obcaecati voluptate possimus quo consectetur aspernatur!
-                </p>
-              </div>
-              <div className={styles["post-stats"]}>
-                <span>like</span>
-                <span>comment</span>
-              </div>
-            </div>
-          </li>
-          <li className={styles["post-margin-bottom"]}></li>
-        </ul>
+        {error && <div>{error}</div>}
+        {isLoading && <div>Loading...</div>}
+        {posts && (
+          <ul className={`${styles["post-list"]} hide-scroll`}>
+            {posts.map((item) => {
+              return <Post key={item.id} post={item} />;
+            })}
+
+            <li className={styles["post-margin-bottom"]}></li>
+          </ul>
+        )}
       </div>
+      {/* <button className={styles["btn-create-post"]} onClick={handleToggleModal}>
+        +
+      </button>
+      <Modal isOpen={isModalOpen} onClose={handleToggleModal}>
+        <div>oi</div>
+      </Modal> */}
     </main>
   );
 };
--- components/MainPage/post/page.tsx
+++ components/MainPage/post/page.tsx
@@ -1,3 +1,37 @@
 import styles from "./post.module.css";
+import { PostContent } from "@/data/posts";
 
-export default function Post() {}
+interface PostProps {
+  post: PostContent;
+}
+export default function Post({ post }: PostProps) {
+  return (
+    <li className={styles.post}>
+      <div className={styles["post-wrapper"]}>
+        <header className={styles["post-header"]}>
+          <div className={styles["post-author"]}>
+            {/* <img
+              src={post.author.profilePic}
+              alt=""
+              className={styles["profile-pic"]}
+              width={30}
+            /> */}
+            {/* <span className={styles.username}>{post.author.username}</span> */}
+            {/* <span>Hora</span> */}
+          </div>
+          {/* <div>
+          Seguir e mais
+        </div> */}
+        </header>
+        <div className={styles["post-content-container"]}>
+          <h3 className={styles["post-title"]}>{post.title}</h3>
+          <p className={styles["post-content"]}>{post.content}</p>
+        </div>
+        <div className={styles["post-stats"]}>
+          <span>like</span>
+          <span>comment</span>
+        </div>
+      </div>
+    </li>
+  );
+}
