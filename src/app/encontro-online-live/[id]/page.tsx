ca4c91de1584bd81c3314a7a42ce2a53bd5f2151
Fabio Leonardo <leonardofafj@gmail.com>
2024-11-29 08:47:51 -0300

    fix bugs

--- src/app/encontro-online-live/[id]/page.tsx
+++ src/app/encontro-online-live/[id]/page.tsx
@@ -1,23 +1,18 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
 import Header from "../../../../components/header/page";
 import Content from "../../../../components/EncontroOnline/content/content";
 import Chat from "../../../../components/EncontroOnline/chat/chat";
 import styles from "./encontro-online-live.module.css";
 import { CardMeetup } from "@/data/meetup";
+import { useRouter } from "next/router";
 
-interface Props {
-  params: {
-    id: number | string;
-  };
-}
-
-const App = ({ params }: Props) => {
+const App = ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = React.use(params);
   const [meetup, setMeetup] = useState<CardMeetup | null>(null);
 
   useEffect(function getElement() {
-    fetch("http://localhost:8000/meetup/" + params.id)
+    fetch("http://localhost:8000/meetup/" + id)
       .then((res) => {
         if (res.ok) {
           return res.json();
